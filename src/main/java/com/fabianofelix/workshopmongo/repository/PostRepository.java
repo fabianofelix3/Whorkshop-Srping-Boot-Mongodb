2ffc26ad77839fe8a7acde6f9a832af04ee06e70
fabiano <fabianofelix3@gmail.com>
2019-02-26 19:48:02 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/fabianofelix/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.fabianofelix.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.fabianofelix.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
