90c068cbd5a65d346bd45c71d1db95e7d803d898
fabiano <fabianofelix3@gmail.com>
2019-03-02 16:49:03 -0300

    Obtendo um post por id

---
+++ src/main/java/com/fabianofelix/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.fabianofelix.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabianofelix.workshopmongo.domain.Post;
+import com.fabianofelix.workshopmongo.repository.PostRepository;
+import com.fabianofelix.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
